dd29eee4e8e2e0f1da622d31f6e1185a3b181177
crazycoder1729 <crazycoder1729@gmail.com>
2024-04-11 22:23:09 +0530

    version

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask_cors import CORS
-from cp import prediction
+import pickle
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -23,9 +25,18 @@ def help():
 def submit():
     data = request.json  # Access submitted form data
     # Process the data as neededs
-    temp  = prediction([[data['founded_at'],data['closed_at'],data['funding_rounds'],data['funding_total_usd'],data['country_code']]])
-    
-    return temp
+    input_arr  = [[data['founded_at'],data['closed_at'],data['funding_rounds'],data['funding_total_usd'],data['country_code']]]
+    with open('combined_pipeline.pkl', 'rb') as file:
+        loaded_combined_pipeline = pickle.load(file)
+
+    arr = ['acquired','closed','ipo','operating']
+    np_arr = np.array(input_arr).reshape(-1,5)
+    df_d = pd.DataFrame(np_arr,columns=['founded_at','closed_at','funding_rounds','funding_total_usd','country_code'])
+    df_d['founded_at'] = pd.to_datetime(df_d['founded_at']).dt.year
+    df_d['closed_at'] = pd.to_datetime(df_d['closed_at']).dt.year
+    y = loaded_combined_pipeline.predict(df_d)[0]
+    res = arr[y]
+    return res
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=4000)
